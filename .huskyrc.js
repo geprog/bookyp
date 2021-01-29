ffee72768c3e3857a1c58f1a4996e53b7814d03c
Lukas Hass <lukas@slucky.de>
2021-01-29 14:50:17 +0100

    style: format all files with prettier

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,8 +1,8 @@
 /** eslint-env node */
 module.exports = {
-  "hooks": {
-    "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
-    "pre-commit": "yarn lint:changed",
-    "pre-push": "yarn test:changed",
+  hooks: {
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+    'pre-commit': 'yarn lint:changed',
+    'pre-push': 'yarn test:changed',
   },
 };
