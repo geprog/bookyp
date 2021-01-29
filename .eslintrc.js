fb205aa70cb699c3ef7eef177b5b18c7dde8736e
Lukas Hass <lukas@slucky.de>
2021-01-29 14:12:05 +0100

    chore: disable typescript rules for all .js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,5 +32,14 @@ module.exports = {
       },
       extends: ['plugin:jest/all'],
     },
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
   ],
 };
--- packages/backend/buildscript.js
+++ packages/backend/buildscript.js
@@ -1,7 +1,4 @@
 /** eslint-env node */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-var-requires */
 const esbuild = require('esbuild');
 const pnpPlugin = require('esbuild-plugin-pnp');
 
