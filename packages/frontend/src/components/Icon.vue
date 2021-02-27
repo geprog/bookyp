56bd29aca77dd6c151e2aa55eab90a1027afa766
Anton Bracke <a.bracke@geprog.com>
2021-02-27 09:39:03 +0000

    feat(frontend): :sparkles: add icon component

---
+++ packages/frontend/src/components/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <img :src="`./src/assets/icons/${name}.svg`" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Icon',
+
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
