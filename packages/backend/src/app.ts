23c53610d18251a2be3309ca635494c9e73dd25b
Raphael Schwinger <raphaelschwinger@gmail.com>
2021-03-05 12:17:44 +0000

    fix(backend): :bug: configure services after configuring socketio
    Services must be configured after the socket so that they are acessible from the socket!

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,12 +1,8 @@
 import { createApplication } from '@bookyp/core';
 import socketio from '@feathersjs/socketio';
-
 import services from './services';
 
 const app = createApplication();
-
-// Set up our services (see `services/index.js`)
-app.configure(services);
 
 // Configure Socket.io real-time APIs
 app.configure(
@@ -16,4 +12,6 @@ app.configure(
   }),
 );
 
+// Set up our services (see `services/index.js`)
+app.configure(services);
 export default app;
