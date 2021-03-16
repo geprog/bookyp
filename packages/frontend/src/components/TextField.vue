66ee62624b7e637a0cfb1af3d5a847a75947aee3
Lukas Hass <lukas@slucky.de>
2021-03-16 18:19:38 +0000

    feat: add InputField component
    move padding and border out of TextField

--- packages/frontend/src/components/TextField.vue
+++ packages/frontend/src/components/TextField.vue
@@ -1,7 +1,7 @@
 <template>
   <input
     v-model="innerValue"
-    class="w-full py-2 text-gray-900 placeholder-gray-600 border-b-1 focus:outline-none"
+    class="w-full text-gray-900 placeholder-gray-600 focus:outline-none"
     type="text"
     :placeholder="placeholder"
   />
