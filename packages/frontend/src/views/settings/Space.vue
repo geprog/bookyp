9bdf68b9ec61447d7da9d8bc93d2e6e11cb309e3
Raphael Schwinger <r.schwinger@geprog.com>
2021-05-03 15:00:46 +0000

    feat(frontend): admin - add elements to the map
    ## Summary
    
    Adds a new sample `mapObject` to the `Space`. First click on the new button, then click on `Space` to position it and then click on save. Adds `channels.ts` to communicate service events with all connected and authenticated clients.

--- packages/frontend/src/views/settings/Space.vue
+++ packages/frontend/src/views/settings/Space.vue
@@ -1,35 +1,57 @@
 <template>
   <Header :title="t('settings')" has-back>
+    <button v-if="editing" data-test="save-button" type="submit" @click="saveNewMapObject">
+      <Icon name="check-mark" />
+    </button>
     <template #second>
       <SettingsTabs />
     </template>
   </Header>
   <div class="mx-4 mt-4">
-    <svg class="w-full h-128" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="w-full h-128" fill="none" xmlns="http://www.w3.org/2000/svg" @click="positionNewMapObject">
       <path v-for="path in floorPlan" :key="path" :d="path" stroke="#323130" stroke-width="2" />
       <g v-for="mapObject in mapObjects" :key="mapObject" :transform="`translate(${mapObject.xPos},${mapObject.yPos})`">
         <path v-for="path in mapObject.paths" :key="path" :d="path" stroke="#323130" stroke-width="1" />
       </g>
+      <g v-if="newMapObject" :transform="`translate(${newMapObject.xPos},${newMapObject.yPos})`">
+        <path
+          v-for="path in newMapObject.paths"
+          :key="path"
+          :d="path"
+          fill="#FDE68A"
+          stroke="#F59E0B"
+          stroke-width="2"
+        />
+      </g>
     </svg>
+    <FloatingButton data-test="add-button" class="fixed bottom-8 right-8" icon-name="table" @click="addMapObject" />
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { Model } from '@bookyp/core';
+import { computed, defineComponent, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
+import FloatingButton from '~/components/FloatingButton.vue';
 import Header from '~/components/Header.vue';
+import Icon from '~/components/Icon.vue';
 import SettingsTabs from '~/components/SettingsTabs.vue';
+import useFeathers from '~/compositions/useFeathers';
 import useFind from '~/compositions/useFind';
 
 export default defineComponent({
   name: 'Space',
 
-  components: { Header, SettingsTabs },
+  components: { FloatingButton, Icon, Header, SettingsTabs },
 
   setup() {
     // eslint-disable-next-line @typescript-eslint/unbound-method
     const { t } = useI18n();
+    const feathers = useFeathers();
+
+    // flag to show if we are currently editing the map
+    const editing = computed(() => !!newMapObject.value);
     const { data: mapObjects } = useFind('mapObjects');
 
     const { data: spaces } = useFind('spaces');
@@ -40,7 +62,48 @@ export default defineComponent({
       return spaces.value[0].floorPlan;
     });
 
-    return { t, floorPlan, mapObjects };
+    let newMapObject = ref<null | Omit<Model.MapObject, '_id'>>(null);
+
+    const addMapObject = () => {
+      newMapObject.value = {
+        xPos: 0,
+        yPos: 0,
+        rotation: 0,
+        paths: [
+          'M56.9259 1.12463H17.0648V83.4525H56.9259V1.12463Z',
+          'M17.0648 26.6198H1.12036V58.4886H17.0648V26.6198Z',
+        ],
+
+        type: Model.MapObjectTypes.table,
+      };
+    };
+
+    async function saveNewMapObject(): Promise<void> {
+      // save newMapObject
+      if (newMapObject.value) {
+        await feathers.service('mapObjects').create(newMapObject.value);
+        newMapObject.value = null;
+      }
+    }
+
+    function positionNewMapObject(event: MouseEvent) {
+      const svg = event.target as SVGSVGElement;
+      const pt = svg.createSVGPoint();
+
+      // pass event coordinates
+      pt.x = event.clientX;
+      pt.y = event.clientY;
+
+      // transform to SVG coordinates
+      const matrix = svg.getScreenCTM()?.inverse();
+      const svgP = pt.matrixTransform(matrix);
+      if (newMapObject.value) {
+        newMapObject.value.xPos = svgP.x;
+        newMapObject.value.yPos = svgP.y;
+      }
+    }
+
+    return { t, editing, floorPlan, mapObjects, addMapObject, newMapObject, positionNewMapObject, saveNewMapObject };
   },
 });
 </script>
--- packages/frontend/test/views/Space.test.ts
+++ packages/frontend/test/views/Space.test.ts
@@ -1,4 +1,6 @@
-import { shallowMount } from '@vue/test-utils';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { mount, shallowMount } from '@vue/test-utils';
+import toDiffableHtml from 'diffable-html';
 import { ref } from 'vue';
 
 import useFeathers from '~/compositions/useFeathers';
@@ -10,6 +12,28 @@ jest.mock('~/compositions/useFind');
 jest.mock('vue-i18n');
 
 describe('Space component', () => {
+  it('should render correctly', () => {
+    // given
+    // mock useFeathers
+    const useFeathersMock = {
+      service: () => ({
+        find: jest.fn(() => []),
+        create: jest.fn(),
+      }),
+    };
+    (useFeathers as jest.Mock).mockReturnValue(useFeathersMock);
+
+    // mock useFind
+    const useFindMock = {
+      data: ref([]),
+    };
+    (useFind as jest.Mock).mockReturnValue(useFindMock);
+    // when
+    const wrapper = shallowMount(Space, {});
+
+    // then
+    expect(toDiffableHtml(wrapper.html())).toMatchSnapshot();
+  });
   it('should display a floorPlan', async () => {
     expect.assertions(2);
     // given
@@ -43,5 +67,132 @@ describe('Space component', () => {
     // then
     expect(wrapper.findAll('path')[0].attributes('d')).toStrictEqual(floorPlan[0]);
     expect(wrapper.findAll('path')[1].attributes('d')).toStrictEqual(floorPlan[1]);
+  });
+  it('should trigger addMapObjectFunction if clicking the add button', () => {
+    // given
+    // mock useFeathers
+    const useFeathersMock = {
+      service: () => ({
+        find: jest.fn(() => []),
+        create: jest.fn(),
+      }),
+    };
+    (useFeathers as jest.Mock).mockReturnValue(useFeathersMock);
+    // mock useFind
+    const useFindMock = {
+      data: ref([]),
+    };
+    (useFind as jest.Mock).mockReturnValue(useFindMock);
+    // when
+    const wrapper = shallowMount(Space, {});
+    void wrapper.find('[data-test=add-button]').trigger('click');
+    // then
+    expect(wrapper.vm.editing).toBe(true);
+    expect(wrapper.vm.newMapObject).not.toBe(false);
+  });
+  it('should not render save button', () => {
+    expect.assertions(1);
+    // given
+    // mock useFeathers
+    const useFeathersMock = {
+      service: () => ({
+        find: jest.fn(() => []),
+        create: jest.fn(),
+      }),
+    };
+    (useFeathers as jest.Mock).mockReturnValue(useFeathersMock);
+    // mock useFind
+    const useFindMock = {
+      data: ref([]),
+    };
+    (useFind as jest.Mock).mockReturnValue(useFindMock);
+    // when
+    const wrapper = shallowMount(Space, {});
+    // then
+    expect(wrapper.find('[data-test=save-button]').exists()).toBe(false);
+  });
+  it('should render save button when user clicked on add-table-button', async () => {
+    expect.assertions(1);
+    // given
+    // mock useFeathers
+    const useFeathersMock = {
+      service: () => ({
+        find: jest.fn(() => []),
+        create: jest.fn(),
+      }),
+    };
+    (useFeathers as jest.Mock).mockReturnValue(useFeathersMock);
+    // mock useFind
+    const useFindMock = {
+      data: ref([]),
+    };
+    const mockRoute = {
+      push: jest.fn(),
+    };
+    const mockRouter = {
+      push: jest.fn(),
+    };
+    (useFind as jest.Mock).mockReturnValue(useFindMock);
+    // when
+    const wrapper = mount(Space, {
+      global: {
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter,
+        },
+      },
+    });
+    await wrapper.find('[data-test=add-button]').trigger('click');
+    await wrapper.vm.$nextTick();
+    // then
+    expect(wrapper.find('[data-test=save-button]').exists()).toBe(true);
+  });
+  it('should save newMapObject when clicking save', async () => {
+    expect.assertions(2);
+    // given
+    const useFeathersServiceMock = {
+      find: jest.fn(() => []),
+      create: jest.fn(),
+    };
+    // mock useFeathers
+    const useFeathersMock = {
+      service: () => useFeathersServiceMock,
+    };
+    (useFeathers as jest.Mock).mockReturnValue(useFeathersMock);
+    // mock useFind
+    const useFindMock = {
+      data: ref([]),
+    };
+    const mockRoute = {
+      push: jest.fn(),
+    };
+    const mockRouter = {
+      push: jest.fn(),
+    };
+    (useFind as jest.Mock).mockReturnValue(useFindMock);
+    // when
+    const wrapper = mount(Space, {
+      global: {
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter,
+        },
+      },
+    });
+    await wrapper.find('[data-test=add-button]').trigger('click');
+    await wrapper.vm.$nextTick();
+    await wrapper.find('[data-test=save-button]').trigger('click');
+    await wrapper.vm.$nextTick();
+    // then
+    expect(wrapper.find('[data-test=save-button]').exists()).toBe(false);
+    expect(useFeathersServiceMock.create).toHaveBeenCalledWith(
+      expect.objectContaining({
+        paths: expect.any(Array),
+        rotation: expect.any(Number),
+        type: expect.any(String),
+        xPos: expect.any(Number),
+        yPos: expect.any(Number),
+      }),
+    );
   });
 });
