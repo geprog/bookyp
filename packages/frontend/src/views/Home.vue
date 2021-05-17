c7e83b399bbaf4b63d897b881e98d4abf0db797e
Anton Bracke <a.bracke@geprog.com>
2021-05-17 12:21:12 +0000

    feat: update home view

--- packages/frontend/src/views/Home.vue
+++ packages/frontend/src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
-  <Header title="Home" has-back>
-    <IconButton icon="settings" @click="$router.push({ name: 'settings-bookables' })" />
+  <Header :title="t('bookyp').toUpperCase()" has-logo>
+    <template #start>
+      <BookypIcon />
+    </template>
+    <IconButton icon="settings" @click="$router.push({ name: 'settings-space' })" />
     <IconButton icon="logout" @click="logoutFunc" />
   </Header>
-  <div class="home">
-    <p v-if="user">{{ t('hello') }} {{ user.name }}</p>
-  </div>
+
   <div v-if="bookables">
     <ListItem
       v-for="bookable in bookables"
       :key="bookable._id"
       :label="bookable.name"
       status-color="bg-primary-normal"
+      class="cursor-pointer"
       :description="bookable.description"
       @click="$router.push({ name: 'booking-create', params: { bookableId: bookable._id } })"
     />
@@ -23,15 +25,18 @@ import { defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
 
+import BookypIcon from '~/assets/icons/bookyp.svg';
 import IconButton from '~/components/buttons/IconButton.vue';
 import Header from '~/components/Header.vue';
 import ListItem from '~/components/ListItem.vue';
-import { logout, user } from '~/compositions/useAuthentication';
+import { logout } from '~/compositions/useAuthentication';
 import useFind from '~/compositions/useFind';
 
 export default defineComponent({
   name: 'Home',
-  components: { Header, ListItem, IconButton },
+
+  components: { Header, ListItem, IconButton, BookypIcon },
+
   setup() {
     const router = useRouter();
     // eslint-disable-next-line @typescript-eslint/unbound-method
@@ -42,7 +47,8 @@ export default defineComponent({
       await logout();
       await router.push({ name: 'loading-screen' });
     };
-    return { t, user, logoutFunc, bookables };
+
+    return { t, logoutFunc, bookables };
   },
 });
 </script>
