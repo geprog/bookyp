dd522479e7fd179e0174d138e75f3e35de918351
Anton Bracke <a.bracke@geprog.com>
2021-05-17 11:27:14 +0000

    feat: add default slot for list-item

--- packages/frontend/src/components/ListItem.vue
+++ packages/frontend/src/components/ListItem.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex mx-3 my-2 p-3 rounded-lg shadow-full bg-white">
     <div :class="statusColor" class="w-2 rounded flex-shrink-0" />
-    <div class="flex flex-col ml-3 overflow-hidden min-w-0">
-      <label v-if="label" class="mt-1 mb-2 text-base truncate">{{ label }}</label>
-      <p v-if="description" class="w-full text-gray-700 text-sm truncate">{{ description }}</p>
+    <div class="flex flex-col ml-3 overflow-hidden min-w-0 flex-grow">
+      <slot>
+        <label v-if="label" class="mt-1 mb-2 text-base truncate">{{ label }}</label>
+        <p v-if="description" class="w-full text-gray-700 text-sm truncate">{{ description }}</p>
+      </slot>
     </div>
   </div>
 </template>
