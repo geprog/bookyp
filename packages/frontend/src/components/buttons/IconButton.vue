efef6093d1e643ca6d4ac7ff6b00f4bcc26674fa
Anton Bracke <a.bracke@geprog.com>
2021-06-11 15:17:47 +0000

    fix: adjust hover effect to match design (!308)

--- packages/frontend/src/components/buttons/IconButton.vue
+++ packages/frontend/src/components/buttons/IconButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="button"
-    class="flex items-center justify-center h-9 w-9 bg-white rounded-full focus:outline-transparent hover:shadow-md hover:text-primary-dark disabled:cursor-not-allowed disabled:bg-gray-background disabled:text-white"
+    class="flex items-center justify-center h-9 w-9 bg-white rounded-full focus:outline-transparent hover:text-primary-dark disabled:cursor-not-allowed disabled:bg-gray-background disabled:text-white"
     :disabled="disabled"
   >
     <Icon data-test="button-icon" :name="icon" />
