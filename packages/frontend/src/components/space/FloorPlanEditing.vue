11683d5041380c9131f8f1640ddcf43ea4b46798
Raphael Schwinger <r.schwinger@geprog.com>
2021-07-23 14:10:04 +0000

    feat(frontend): space editor - add new wall (!260)

---
+++ packages/frontend/src/components/space/FloorPlanEditing.vue
@@ -0,0 +1,23 @@
+<template>
+  <path v-for="path in floorPlan" :key="path" :d="path" class="stroke-black" stroke-width="2" />
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, toRef } from 'vue';
+
+import { useAndRegisterViewBox } from '~/compositions/space/useViewBox';
+
+export default defineComponent({
+  name: 'FloorPlanEditing',
+  props: {
+    floorPlan: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    useAndRegisterViewBox('FloorPlan', toRef(props, 'floorPlan'), { strokeWidth: 2 });
+  },
+});
+</script>
--- packages/frontend/src/views/settings/space/FloorPlanEdit.vue
+++ packages/frontend/src/views/settings/space/FloorPlanEdit.vue
@@ -1,28 +1,49 @@
 <template>
   <div class="m-4 flex flex-col flex-grow min-h-0">
-    <SpaceMap data-test="space-map">
-      <FloorPlan />
+    <SpaceMap
+      data-test="space-map"
+      :class="{ 'cursor-pointer': addingStage !== 'not-started' }"
+      @down-inside-svg="downInsideFloorPlan"
+      @up-inside-svg="upInsideFloorPlan"
+      @move-inside-svg="moveInsideFloorPlan"
+    >
+      <FloorPlanEditing v-if="floorPlan" v-model:floor-plan="floorPlan" />
       <MapObjects />
     </SpaceMap>
-    <div class="ml-auto flex flex-row">
+    <div class="flex flex-row">
+      <div class="mr-auto flex flex-row">
+        <FloatingButton
+          v-if="addingStage === 'not-started'"
+          data-test="add-button"
+          icon="add-circle"
+          @click.stop="clickOnAddButton"
+        />
+      </div>
       <slot name="toggleBar" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import clone from 'lodash/clone';
+import { defineComponent, onMounted, Ref, ref, toRef, watch } from 'vue';
 
-import FloorPlan from '~/components/space/FloorPlan.vue';
+import FloatingButton from '~/components/buttons/FloatingButton.vue';
+import FloorPlanEditing from '~/components/space/FloorPlanEditing.vue';
 import MapObjects from '~/components/space/MapObjects.vue';
 import SpaceMap from '~/components/space/SpaceMap.vue';
+import getCurrentSpace from '~/compositions/space/useCurrentSpace';
+import useNewFloorPlanObject from '~/compositions/space/useNewFloorPlanObject';
+import useFeathers from '~/compositions/useFeathers';
+import { waitUntilDataHasBeenLoaded } from '~/utils';
 
 export default defineComponent({
   name: 'FloorPlanEdit',
   components: {
     SpaceMap,
-    FloorPlan,
+    FloorPlanEditing,
     MapObjects,
+    FloatingButton,
   },
 
   props: {
@@ -41,7 +62,75 @@ export default defineComponent({
 
   emits: {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    changeHappend: (_flag: boolean) => true,
+    'change-happend': (_flag: boolean) => true,
+  },
+
+  setup(props, context) {
+    const saveTrigger = toRef(props, 'saveTrigger');
+    const abortTrigger = toRef(props, 'abortTrigger');
+
+    const feathers = useFeathers();
+    const { data: currentSpace, isLoading } = getCurrentSpace();
+    const floorPlan: Ref<string[]> = ref([]);
+
+    const useNewFloorPlanObjects = useNewFloorPlanObject(floorPlan);
+
+    onMounted(async () => {
+      const loadedCurrentSpace = await waitUntilDataHasBeenLoaded(currentSpace, isLoading);
+      if (loadedCurrentSpace.value !== undefined) {
+        floorPlan.value = clone(loadedCurrentSpace.value.floorPlan);
+      }
+    });
+
+    watch(saveTrigger, async () => {
+      await saveFloorPlan();
+      context.emit('change-happend', false);
+    });
+
+    watch(abortTrigger, () => {
+      if (currentSpace.value !== undefined) {
+        floorPlan.value = clone(currentSpace.value.floorPlan);
+        context.emit('change-happend', false);
+      } else {
+        throw new Error('No current space');
+      }
+    });
+
+    async function saveFloorPlan(): Promise<void> {
+      if (currentSpace.value !== undefined) {
+        const saveSpace = {
+          _id: currentSpace.value?._id,
+          floorPlan: floorPlan.value,
+        };
+        await feathers.service('spaces').update(currentSpace.value._id, saveSpace);
+      }
+    }
+
+    function clickOnAddButton() {
+      context.emit('change-happend', true);
+      useNewFloorPlanObjects.startAddingWall();
+    }
+
+    function downInsideFloorPlan(svgP: DOMPoint) {
+      useNewFloorPlanObjects.addFirstPositionOfWall(svgP);
+    }
+
+    function upInsideFloorPlan(svgP: DOMPoint) {
+      useNewFloorPlanObjects.finishAddingOfWall(svgP);
+    }
+
+    function moveInsideFloorPlan(svgP: DOMPoint) {
+      useNewFloorPlanObjects.updateSecondPositionOfWall(svgP);
+    }
+
+    return {
+      clickOnAddButton,
+      addingStage: useNewFloorPlanObjects.addingStage,
+      downInsideFloorPlan,
+      upInsideFloorPlan,
+      moveInsideFloorPlan,
+      floorPlan,
+    };
   },
 });
 </script>
