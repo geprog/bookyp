328fcf611464e8db211b9bd87bcbdb8318449046
Lukas Hass <lukas@slucky.de>
2021-07-30 12:49:38 +0000

    refactor: seed space with unique id (!337)

---
+++ packages/backend/src/migrations/setExplicitSpaceId.ts
@@ -0,0 +1,30 @@
+import { Migration } from '@geprog/node-migrate-ts';
+import { ObjectId } from 'mongodb';
+
+type Space = {
+  _id: ObjectId;
+  floorPlan: string[];
+};
+
+export const setExplicitSpaceId: Migration = {
+  id: 'set-explicit-space-id',
+  async up(context) {
+    if (!context || !context.db) {
+      throw new Error('Please pass a context with a db object');
+    }
+    const { db } = context;
+
+    const space = await db.collection<Space>('spaces').findOne({});
+    if (space === null) {
+      throw new Error('A space should exist');
+    }
+    await db.collection<Space>('spaces').deleteOne({ _id: space._id });
+    await db.collection<Space>('spaces').insertOne({
+      ...space,
+      _id: new ObjectId('60f53bede6f8313dff7f99e0'),
+    });
+  },
+  async down() {
+    // not implemented since reverting to an unknown id is not possible
+  },
+};
