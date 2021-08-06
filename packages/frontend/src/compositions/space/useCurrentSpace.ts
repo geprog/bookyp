77a379f60253c4596d8578468fa96ed20303735d
Lukas Hass <lukas@slucky.de>
2021-08-06 09:53:38 +0000

    fix: simplify useCurrentSpace (!360)

--- packages/frontend/src/compositions/space/useCurrentSpace.ts
+++ packages/frontend/src/compositions/space/useCurrentSpace.ts
@@ -1,27 +1,19 @@
 import { Model } from '@bookyp/core';
 import { UseGet } from '@geprog/use-feathers';
-import { Ref, ref } from 'vue';
+import { ref } from 'vue';
 
 import useGet from '~/compositions/useGet';
 
 // TODO: remove space id once it can be selected by the user
 export const spaceId = ref('60f53bede6f8313dff7f99e0');
 
-let internalSpace: Ref<Model.Space | undefined>;
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const internalIsLoading: Ref<Ref<boolean>> = ref(ref(false));
-
-let alreadyLoaded = false;
+let currentSpace: UseGet<Model.Space>;
 
 export default function getCurrentSpace(): UseGet<Model.Space> {
-  if (!alreadyLoaded) {
-    const { data, isLoading } = useGet('spaces', spaceId, {
+  if (!currentSpace) {
+    currentSpace = useGet('spaces', spaceId, {
       disableUnloadingEventHandlers: true,
     });
-    internalSpace = data;
-    internalIsLoading.value = isLoading;
-    alreadyLoaded = true;
   }
-  return { data: internalSpace, isLoading: internalIsLoading.value };
+  return currentSpace;
 }
