7634b58ff67533807a0967a8a2ee329d5f8407d8
Lukas Hass <lukas@slucky.de>
2022-01-26 23:40:29 +0000

    feat: hide settings button when not space admin (!419)

--- packages/frontend/src/components/headers/HomeHeader.vue
+++ packages/frontend/src/components/headers/HomeHeader.vue
@@ -7,6 +7,7 @@
     <IconButton data-test="button-account" icon="person" @click="$router.push({ name: 'account-bookings' })" />
     <IconButton data-test="spaces-button" icon="location" @click="$router.push({ name: 'spaces-list' })" />
     <IconButton
+      v-show="isAdmin"
       data-test="button-settings"
       icon="settings"
       @click="$router.push({ name: 'settings-space-map-objects' })"
@@ -16,13 +17,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 import BookypIcon from '~/assets/icons/bookyp.svg';
 import IconButton from '~/components/buttons/IconButton.vue';
 import Header from '~/components/headers/Header.vue';
-import { logout } from '~/compositions/useAuthentication';
+import { useCurrentSpace } from '~/compositions/space/useCurrentSpace';
+import { logout, user } from '~/compositions/useAuthentication';
 
 export default defineComponent({
   name: 'HomeHeader',
@@ -37,7 +39,12 @@ export default defineComponent({
     // eslint-disable-next-line @typescript-eslint/unbound-method
     const { t } = useI18n();
 
-    return { t, logout };
+    const { currentSpace } = useCurrentSpace();
+    const isAdmin = computed(() =>
+      currentSpace.value?.members?.some((member) => member.userId === user.value?._id && member.role === 'admin'),
+    );
+
+    return { t, logout, isAdmin };
   },
 });
 </script>
