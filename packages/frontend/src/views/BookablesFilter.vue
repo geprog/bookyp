8ba0decdce7b4e4ff6c5f47da8575a2d3df42ff7
Lukas Hass <lukas@slucky.de>
2022-05-10 10:34:56 +0000

    feat: use date range picker in filter (!489)

--- packages/frontend/src/views/BookablesFilter.vue
+++ packages/frontend/src/views/BookablesFilter.vue
@@ -3,56 +3,41 @@
     <IconButton type="submit" form="filterBookablesForm" icon="check-mark" />
   </Header>
   <form id="filterBookablesForm" class="my-2 mx-4" @submit.prevent="submitBookablesFilter">
-    <div class="flex flex-row">
-      <div class="flex flex-col flex-grow">
-        <InputField icon-name="play">
-          <DateTimePicker v-model="start" :placeholder="t('start')" :min-date="new Date()" />
-        </InputField>
-        <InputField icon-name="stop">
-          <DateTimePicker v-model="end" :placeholder="t('end')" :min-date="new Date()" />
-        </InputField>
-      </div>
+    <DateRangePicker v-model:start="start" v-model:end="end" :bookings="[]">
       <Button v-if="hasActiveBookablesFilter" icon="dismiss" outlined class="ml-4 px-1" @click="resetBookablesFilter" />
-    </div>
+    </DateRangePicker>
   </form>
 </template>
 
 <script lang="ts">
 import dayjs from 'dayjs';
-import { computed, defineComponent, ref, watch } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
 
 import Button from '~/components/buttons/Button.vue';
 import IconButton from '~/components/buttons/IconButton.vue';
 import Header from '~/components/headers/Header.vue';
-import InputField from '~/components/InputField.vue';
-import DateTimePicker from '~/components/inputs/DateTimePicker.vue';
+import DateRangePicker from '~/components/inputs/DateRangePicker.vue';
 import { useBookablesFilter } from '~/compositions/useBookablesFilter';
 
 export default defineComponent({
   name: 'BookablesFilter',
 
-  components: { Header, IconButton, InputField, DateTimePicker, Button },
+  components: { Header, IconButton, Button, DateRangePicker },
 
   setup() {
     const { t } = useI18n();
     const router = useRouter();
     const { bookablesFilter } = useBookablesFilter();
 
-    const hasActiveBookablesFilter = computed(() => !!bookablesFilter.value);
+    const hasActiveBookablesFilter = computed(() => !bookablesFilter.value?.quickFilterEnabled);
 
     const start = ref<Date>(bookablesFilter.value?.start || new Date());
 
     const defaultEndDate = dayjs(start.value).add(2, 'hour').toDate();
 
     const end = ref<Date>(bookablesFilter.value?.end || defaultEndDate);
-
-    watch(start, (newStart, oldStart) => {
-      end.value = dayjs(newStart)
-        .add(Math.abs(dayjs(oldStart).diff(dayjs(end.value))))
-        .toDate();
-    });
 
     const submitBookablesFilter = () => {
       bookablesFilter.value = {
