7262079a4660176ceed78bc247da331da57117dd
Marius Heine <m.heine@geprog.com>
2022-06-13 19:45:23 +0000

    feat: apply proper scale to map (!521)

--- packages/frontend/src/compositions/space/useNewMapObject.ts
+++ packages/frontend/src/compositions/space/useNewMapObject.ts
@@ -35,7 +35,10 @@ export default function useNewMapObject(
       xPos: 0,
       yPos: 0,
       rotation: 0,
-      paths: ['M56.9259 1.12463H17.0648V83.4525H56.9259V1.12463Z', 'M17.0648 26.6198H1.12036V58.4886H17.0648V26.6198Z'],
+      paths: [
+        'M32 0h80v140h-80v-140z', // for the table
+        'M0 42h32v56h-32v-56z', // for the chair
+      ],
       type: Model.MapObjectTypes.table,
       space: spaceId.value,
     });
