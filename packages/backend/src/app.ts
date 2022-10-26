7d8f8713bffb0e6126f7f867f109bbadb28eee06
Lukas Hass <lukas@slucky.de>
2022-10-26 16:48:36 +0000

    fix: move app configuration into function (!575)

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -9,80 +9,83 @@ import config from '~/config';
 import services from '~/services';
 import SSOLogoutRoute from '~/services/authentication/sso-logout';
 
-const app = express(createApplication());
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const configureApplication = () => {
+  const app = express(createApplication());
 
-// configure oauth
-app.set('host', config().app.host);
-app.set('port', config().app.port);
+  // configure oauth
+  app.set('host', config().app.host);
+  app.set('port', config().app.port);
 
-// Configure Socket.io real-time APIs
-app.configure(
-  socketio({
-    path: '/api/v1/socket',
-    serveClient: false,
-  }),
-);
+  // Configure Socket.io real-time APIs
+  app.configure(
+    socketio({
+      path: '/api/v1/socket',
+      serveClient: false,
+    }),
+  );
 
-app.configure(feathersCasl);
+  app.configure(feathersCasl);
 
-// Set up our services (see `services/index.js`)
-app.configure(services);
+  // Set up our services (see `services/index.js`)
+  app.configure(services);
 
-app.configure(channels);
+  app.configure(channels);
 
-app.use(SSOLogoutRoute());
+  app.use(SSOLogoutRoute());
 
-app.use('/', (_req, res) => {
-  res.send('You found the backend of Bookyp! ;-)');
-});
+  app.use('/', (_req, res) => {
+    res.send('You found the backend of Bookyp! ;-)');
+  });
 
-const memoryServices: string[] = [];
+  const memoryServices: string[] = [];
 
-app.hooks({
-  before: {
-    all: [
-      (ctx) => {
-        if (ctx.path === 'authentication') {
-          return ctx;
-        }
-        return authenticate('jwt')(ctx);
-      },
-      (ctx) => {
-        if (ctx.path === 'authentication') {
-          return ctx;
-        }
-        if (ctx.path === 'users' && ctx.method === 'get') {
-          return ctx;
-        }
-        return authorizeWithFreshAbility(memoryServices.includes(ctx.path) ? 'feathers-memory' : 'feathers-mongoose')(
-          ctx,
-        );
-      },
-    ],
-  },
-  after: {
-    all: [
-      (ctx) => {
-        if (ctx.path === 'authentication') {
-          return ctx;
-        }
-        return authorizeWithFreshAbility(memoryServices.includes(ctx.path) ? 'feathers-memory' : 'feathers-mongoose')(
-          ctx,
-        );
-      },
-    ],
-  },
-  error: {
-    all: [
-      (ctx) => {
-        if (process.env.NODE_ENV === 'production') {
-          return;
-        }
-        // eslint-disable-next-line no-console
-        console.log(`🔥 Error ${ctx.path}.${ctx.method}:`, ctx.error);
-      },
-    ],
-  },
-});
+  app.hooks({
+    before: {
+      all: [
+        (ctx) => {
+          if (ctx.path === 'authentication') {
+            return ctx;
+          }
+          return authenticate('jwt')(ctx);
+        },
+        (ctx) => {
+          if (ctx.path === 'authentication') {
+            return ctx;
+          }
+          if (ctx.path === 'users' && ctx.method === 'get') {
+            return ctx;
+          }
+          return authorizeWithFreshAbility(memoryServices.includes(ctx.path) ? 'feathers-memory' : 'feathers-mongoose')(
+            ctx,
+          );
+        },
+      ],
+    },
+    after: {
+      all: [
+        (ctx) => {
+          if (ctx.path === 'authentication') {
+            return ctx;
+          }
+          return authorizeWithFreshAbility(memoryServices.includes(ctx.path) ? 'feathers-memory' : 'feathers-mongoose')(
+            ctx,
+          );
+        },
+      ],
+    },
+    error: {
+      all: [
+        (ctx) => {
+          if (process.env.NODE_ENV === 'production') {
+            return;
+          }
+          // eslint-disable-next-line no-console
+          console.log(`🔥 Error ${ctx.path}.${ctx.method}:`, ctx.error);
+        },
+      ],
+    },
+  });
 
-export default app;
+  return app;
+};
--- packages/backend/test/app.test.ts
+++ packages/backend/test/app.test.ts
@@ -1,7 +1,11 @@
 import { createApplication } from '@bookyp/core';
+import socketio from '@feathersjs/socketio';
+
+import { configureApplication } from '~/app';
 
 import { prepareAppMock } from './__helpers__/mocks';
 
+vi.mock('@feathersjs/socketio');
 vi.mock('@bookyp/core', () => ({
   createApplication: vi.fn().mockImplementation(() => prepareAppMock()),
   Model: {
@@ -10,28 +14,26 @@ vi.mock('@bookyp/core', () => ({
 }));
 
 describe('App', () => {
-  it('should use @bookyp/core createApplication', async () => {
+  it('should use @bookyp/core createApplication', () => {
     expect.assertions(1);
     // given
 
     // when
-    await import('~/app');
+    configureApplication();
 
     // then
     expect(createApplication).toHaveBeenCalledTimes(1);
   });
 
-  it('should init a socket with @feathersjs/socketio', async () => {
+  it('should init a socket with @feathersjs/socketio', () => {
     expect.assertions(1);
     // given
-    vi.mock('@feathersjs/socketio');
-    const socketio = await import('@feathersjs/socketio');
 
     // when
-    await import('~/app');
+    configureApplication();
 
     // then
-    expect(socketio.default).toHaveBeenCalledWith({
+    expect(socketio).toHaveBeenCalledWith({
       path: '/api/v1/socket',
       serveClient: false,
     });
