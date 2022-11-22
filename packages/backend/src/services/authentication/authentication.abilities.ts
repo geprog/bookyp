6ab677df400a3e4c0b6192260281cefb399c08f1
Anton Bracke <a.bracke@geprog.com>
2022-11-22 08:26:39 +0000

    fix: correct access to space as member (!597)

--- packages/backend/src/services/authentication/authentication.abilities.ts
+++ packages/backend/src/services/authentication/authentication.abilities.ts
@@ -9,16 +9,17 @@ const defineRulesFor = async (
   // also see https://casl.js.org/v5/en/guide/define-rules
 
   // eslint-disable-next-line @typescript-eslint/unbound-method
-  const { can, rules } = new AbilityBuilder(Ability);
+  const { can, cannot, rules } = new AbilityBuilder(Ability);
 
   const publicSpaces = (await app.service('spaces').find({
     query: { plan: 'public' },
+    paginate: false,
   })) as Model.Space[];
 
   const publicSpaceIds = publicSpaces.map((s) => s._id.toString());
 
   // read access public spaces for everyone (authorized and unauthorized)
-  can('read', 'spaces', ['_id', 'floorPlan', 'name', 'description', 'address'], {
+  can('read', 'spaces', ['_id', 'floorPlan', 'name', 'description', 'address', 'plan'], {
     plan: 'public',
   });
   can('read', 'mapObjects', { space: { $in: publicSpaceIds } });
@@ -39,41 +40,51 @@ const defineRulesFor = async (
       return spacesWhereUserHasRole.map((space) => space._id.toString());
     };
 
-    const spaceIdsUser = getSpaceIds('user');
-    const spaceIdsAdmin = getSpaceIds('admin');
-
-    // get your own user
+    // access your own user
     can('get', 'users', { _id: user._id });
 
-    // access spaces where you are a member of
-    can('read', 'spaces', ['_id', 'floorPlan', 'name', 'description', 'address'], {
-      members: { $elemMatch: { role: 'user', userId: user._id } },
-    });
+    // as a user you can create spaces
+    can('create', 'spaces');
+
+    // access own bookings & invitations
     can(['read', 'remove'], 'bookings', { bookedBy: user._id });
     can(['read', 'remove'], 'invitations', { email: user.email });
-    can(['create', 'update'], 'bookings', { bookedBy: user._id, space: { $in: spaceIdsUser } });
+
+    // write access to public spaces
     can(['create', 'update'], 'bookings', { bookedBy: user._id, space: { $in: publicSpaceIds } });
+
+    // access to spaces where you are a member of
+    const spaceIdsUser = getSpaceIds('user');
+    can('read', 'spaces', ['_id', 'floorPlan', 'name', 'description', 'address', 'plan'], {
+      members: { $elemMatch: { role: 'user', userId: user._id } },
+    });
+    can('read', 'mapObjects', { space: { $in: spaceIdsUser } });
+    can('read', 'bookables', { space: { $in: spaceIdsUser } });
+    can(['create', 'update'], 'bookings', { bookedBy: user._id, space: { $in: spaceIdsUser } });
     can('read', 'bookings', ['_id', 'start', 'end', 'bookable', 'space'], {
       bookedBy: { $ne: user._id },
       space: { $in: spaceIdsUser },
     });
 
-    // as a user you can create spaces
-    can('create', 'spaces');
-
-    // admin access to spaces
+    // admin access to spaces you are an admin of
+    const spaceIdsAdmin = getSpaceIds('admin');
+    // this cannot rule ensures that admins work properly for public spaces
+    cannot('read', 'spaces', ['_id', 'floorPlan', 'name', 'description', 'address', 'plan'], {
+      members: { $elemMatch: { role: 'admin', userId: user._id } },
+      plan: 'public',
+    });
     can(['read', 'delete'], 'spaces', { members: { $elemMatch: { role: 'admin', userId: user._id } } });
     can('update', 'spaces', ['floorPlan', 'members', 'name', 'description', 'address', 'deleted'], {
       members: { $elemMatch: { role: 'admin', userId: user._id } },
     });
-    can(['read', 'create', 'update', 'remove'], 'bookables', { space: { $in: spaceIdsAdmin } });
     can(['read', 'create', 'update', 'remove'], 'mapObjects', { space: { $in: spaceIdsAdmin } });
+    can(['read', 'create', 'update', 'remove'], 'bookables', { space: { $in: spaceIdsAdmin } });
     can('read', 'bookings', ['_id', 'start', 'end', 'bookable', 'space', 'bookedBy'], {
       bookedBy: { $ne: user._id },
       space: { $in: spaceIdsAdmin },
     });
-    can(['read', 'create', 'remove', 'update'], 'invitations', { spaceId: { $in: spaceIdsAdmin } });
     can(['create', 'update'], 'bookings', { bookedBy: user._id, space: { $in: spaceIdsAdmin } });
+    can(['read', 'create', 'remove', 'update'], 'invitations', { spaceId: { $in: spaceIdsAdmin } });
   }
 
   return rules;
