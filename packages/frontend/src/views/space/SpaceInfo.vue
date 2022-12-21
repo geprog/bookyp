6d3a1063afec7fef2e483a2bb4004a482cddebba
Jane Schönfeld <j.schoenfeld@geprog.com>
2022-12-21 12:54:20 +0000

    feat: Only show the fields which are filled in space information (!633)

--- packages/frontend/src/views/space/SpaceInfo.vue
+++ packages/frontend/src/views/space/SpaceInfo.vue
@@ -2,13 +2,13 @@
   <Header has-back :title="t('space_information')" />
   <AppContent>
     <div v-if="space">
-      <LabelField icon-name="home">
+      <LabelField v-if="space.name" icon-name="home">
         <TextField v-model="space.name" readonly data-test="space-name" :placeholder="t('name')" />
       </LabelField>
-      <LabelField icon-name="location">
+      <LabelField v-if="space.address" icon-name="location">
         <TextField v-model="space.address" readonly data-test="space-address" />
       </LabelField>
-      <LabelField icon-name="text-box">
+      <LabelField v-if="space.description" icon-name="text-box">
         <TextField v-model="space.description" readonly data-test="space-description" :placeholder="t('description')" />
       </LabelField>
     </div>
