c529994996d50ad2bcd76a6572ddb6053efa9b72
Jane Schönfeld <j.schoenfeld@geprog.com>
2023-03-13 14:07:56 +0000

    feat: truncate text at 5 lines (!690)

--- packages/frontend/tailwind.config.ts
+++ packages/frontend/tailwind.config.ts
@@ -2,6 +2,7 @@ import colors from 'windicss/colors';
 import { defineConfig } from 'windicss/helpers';
 import plugin from 'windicss/plugin';
 import aspectRatio from 'windicss/plugin/aspect-ratio';
+import lineClamp from 'windicss/plugin/line-clamp';
 import typography from 'windicss/plugin/typography';
 
 export default defineConfig({
@@ -64,5 +65,6 @@ export default defineConfig({
       addUtilities(newUtilities);
     }),
     aspectRatio,
+    lineClamp,
   ],
 });
