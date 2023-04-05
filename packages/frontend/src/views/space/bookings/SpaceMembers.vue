fa659253cbe86bff81bed35113fb3f129bd71ccf
Jane Schönfeld <j.schoenfeld@geprog.com>
2023-04-05 10:13:14 +0000

    fix: show newest booking (!693)

--- packages/frontend/src/views/space/bookings/SpaceMembers.vue
+++ packages/frontend/src/views/space/bookings/SpaceMembers.vue
@@ -41,20 +41,19 @@ const { data: bookings } = useFind(
   'bookings',
   computed(() => ({
     query: {
-      spaceId: currentSpace.value?._id,
+      space: currentSpace.value?._id,
     },
   })),
 );
 
 const newestBookingOfMembers = computed(() =>
-  bookings.value
-    .filter((booking) => booking.space === currentSpace.value?._id)
-    .reduce((acc, booking) => {
-      if (!booking.bookedBy) {
-        return acc;
-      }
-      return acc;
-    }, new Map<string, Model.Booking>()),
+  bookings.value.reduce((acc, booking) => {
+    const bookedBy = acc.get(booking.bookedBy);
+    if (!bookedBy || bookedBy.start < booking.start) {
+      acc.set(booking.bookedBy, booking);
+    }
+    return acc;
+  }, new Map<string, Model.Booking>()),
 );
 
 const spaceMembersWithABookingIDs = computed(() => [
@@ -77,7 +76,6 @@ const spaceMembersWithTheirNewestBooking = computed(() =>
   spaceMembersWithABooking.value.map((member) => {
     const newestBooking = newestBookingOfMembers.value.get(member._id);
     const newestBookingDate = newestBooking ? dayjs(newestBooking.start).format('DD.MM.YYYY') : undefined;
-
     return {
       ...member,
       newestBookingDate,
