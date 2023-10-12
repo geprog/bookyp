b1223a5802c61116c98a42c0d78728c34d27a83a
Anton Bracke <a.bracke@geprog.com>
2023-10-12 07:32:22 +0000

    fix: correct some layout parts (!819)

--- packages/frontend/src/components/layout/AppContent.vue
+++ packages/frontend/src/components/layout/AppContent.vue
@@ -1,7 +1,5 @@
 <template>
   <main class="w-full max-w-3xl flex-grow flex flex-col px-2 md:px-4 md:mx-auto mt-4">
-    <div class="w-full h-14 flex-shrink-0" />
-
     <slot />
   </main>
 </template>
